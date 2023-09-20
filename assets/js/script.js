748cb9136270d9d9688979660035371085367054
graficoPrelude <grafico@preludeadv.it>
2023-09-20 12:41:27 -0400

    scriptong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 console.log('ciao fra')
-const keyboard = document.getElementById('numbers');
-const keys = keyboard.getElementsByTagName('button');
 
-keys.forEach(element => {
-    console.log(element.textContent)
-});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const keyboard = document.getElementById('numbers'),
+          keys = keyboard.getElementsByTagName('button'),
+          array = [];
+    
+    keys.forEach(key => {
+        key.addEventListener("click", (event) => {
+
+            array.push(key.innerText)
+            if(array.length && array.slice(-2).toString() == "0,0"){
+                console.log(array)
+            }
+        })
+    });
+  });
