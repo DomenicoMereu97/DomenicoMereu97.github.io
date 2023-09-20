ca1a3900a43b97085287675936a6cc0ba7f703c9
graficoPrelude <grafico@preludeadv.it>
2023-09-20 13:13:05 -0400

    ss

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,22 @@ import data from '/assets/data.json' assert {type: 'json'};
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const keyboard = document.getElementById('numbers'),
-          keys = keyboard.getElementsByTagName('button'),
-          array = [];
-    
+        keys = keyboard.getElementsByTagName('button'),
+        array = [];
+
     [...keys].forEach(key => {
         key.addEventListener("click", (event) => {
 
             array.push(key.innerText)
-            if(array.length && array.slice(-2).toString() == "0,0"){
-                let accuracy = 1000;
-                const males = data.male.mostUsed.filter(e => e.count > 1000),
-                 females = data.female.mostUsed.filter(e => e.count > 1000),
-
-                names = males.concat(females);
+            if (array.length && array.slice(-2).toString() == "0,0") {
+                const accuracy = 1000,
+                    males = data.male.mostUsed.filter(e => e.count > accuracy),
+                    females = data.female.mostUsed.filter(e => e.count > accuracy),
+                    names = males.concat(females),
+                    [nameLenght] = array;
                 console.log(names.length)
+                console.log(names.filter(e => e.name.length == nameLenght))
             }
         })
     });
-  });
+});
